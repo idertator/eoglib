74f6ce5ec11d3a61521e3e99d70bcd6c04d924f4
Roberto Antonio Becerra García <rbecerra@topgroupexpress.com>
2020-05-11 18:03:01 +0200

    Finished saccade identification by velocities

---
+++ eoglib/identification/__init__.py
@@ -0,0 +1,6 @@
+from .saccades import identify_by_velocity as identify_saccades_by_velocity
+
+
+__all__ = [
+    'identify_saccades_by_velocity'
+]
--- eoglib/identification/saccades.py
+++ eoglib/identification/saccades.py
@@ -1,11 +1,39 @@
 from typing import Iterable
 
-from numpy import abs, argwhere, array
+from numpy import abs, where, array
+from sklearn.cluster import KMeans
 
 from eoglib.models import Saccade
 
 
-def velocity_threshold_identification(velocities: array, threshold: float = 30.0) -> Iterable[Saccade]:
+def _mask_identification(mask: array) -> Iterable[Saccade]:
+    """Identify saccadic movement from a masked array
+
+    In the array:
+        True: The sample belong to a potential saccade
+        False: The sample belong to a potential fixation
+
+    Args:
+        mask: Mask array of boolean values
+    Yields:
+        Saccade objects
+    """
+    first_fixation = where(mask == False)[0][0]
+
+    m0 = mask[first_fixation:-1]
+    m1 = mask[first_fixation+1:]
+
+    onsets = (where((m0 ^ m1) & m1) + first_fixation + 1).ravel()
+    offsets = (where((m0 ^ m1) & m0) + first_fixation + 1).ravel()
+
+    for onset, offset in zip(onsets, offsets):
+        yield Saccade(
+            onset=onset,
+            offset=offset
+        )
+
+
+def threshold_identification(velocities: array, threshold: float = 30.0, **kwargs) -> Iterable[Saccade]:
     """Traditional velocity threshold saccade identification
 
     Args:
@@ -15,13 +43,48 @@ def velocity_threshold_identification(velocities: array, threshold: float = 30.0
         Saccade objects
     """
     mask = abs(velocities) >= threshold
-    m0 = mask[:-1]
-    m1 = mask[1:]
-    onsets = (argwhere((m0 ^ m1) & m1) + 1).ravel()
-    offsets = (argwhere((m0 ^ m1) & m0) + 1).ravel()
-    for onset, offset in zip(onsets, offsets):
-        yield Saccade(
-            onset=onset,
-            offset=offset
-        )
+    yield from _mask_identification(mask)
+
+
+def kmeans_identification(velocities: array, **kwargs) -> Iterable[Saccade]:
+    """Identify impulses from velocity profiles in eye movement signals
+
+    This method identify impulses using the KMeans clustering algorithm.
+    The idea behind this method is try to separate high velocity samples from low velocity ones into 2 clusters using KMeans.
+
+    Contrary to Nyström approach we set the onset and offset points when the velocity cannot decrease no more, so there is no
+    need for thresholds.
+
+    Args: 
+        velocities: Velocities profile of the eye movement
+    Yields:
+        Saccade objects
+    """
+    estimator = KMeans(n_clusters=2)
+    abs_velocities = abs(velocities)
+    labels = estimator.fit_predict(abs_velocities.reshape((len(abs_velocities), 1)))
+
+    inverted = abs_velocities[labels == 1].mean() < abs_velocities[labels == 0].mean()
+    if inverted:
+        labels = abs(labels - 1)
+
+    mask = labels == 1
+    yield from _mask_identification(mask)
+
+
+def identify_by_velocity(velocities: array, method: str='kmeans', **methodArgs) -> Iterable[Saccade]:
+    """Identify saccadic impulses using the velocity profile
+
+    Args: 
+        velocities: Velocities profile of the eye movement
+        method: Method used for perform the identification. Options ['threshold', 'kmeans']
+    Yields:
+        Saccade objects
+    """ 
+    method_func = {
+        'kmeans': kmeans_identification,
+        'threshold': threshold_identification,
+    }.get(method, kmeans_identification)
+
+    yield from method_func(velocities, **methodArgs)
 
